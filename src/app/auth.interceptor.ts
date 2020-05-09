ecbd69fcdc6a915a8251bb27928b9435dd2500dc
Matthieu Corageoud <matthieu.corageoud@gmail.com>
2020-05-09 22:27:38 +0200

    Allow to bypass management of errors in HTTP interceptor

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpResponse, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { throwError, Observable } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
@@ -8,12 +8,23 @@ import { TokenService } from './services/token.service';
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
+	public static HEADER_SKIP_ERROR_HANDLING = 'X-Interceptor-Skip-Error-Handling';
+
 	constructor(private router: Router) {
 	}
 
 	intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-		//enhance request with jwt token
+		const flags = {
+			HEADER_SKIP_ERROR_HANDLING: false
+		};
 		let enhanced_request;
+		//retrieve flags if any
+		if(request.headers.has(AuthInterceptor.HEADER_SKIP_ERROR_HANDLING)) {
+			flags[AuthInterceptor.HEADER_SKIP_ERROR_HANDLING] = true;
+			const headers = request.headers.delete(AuthInterceptor.HEADER_SKIP_ERROR_HANDLING);
+			enhanced_request = request.clone({headers});
+		}
+		//enhance request with jwt token
 		const token = localStorage.getItem(TokenService.tokenStorageKey);
 		if(token) {
 			enhanced_request = request.clone({headers: request.headers.set('Authorization', token)});
@@ -21,19 +32,26 @@ export class AuthInterceptor implements HttpInterceptor {
 		else {
 			enhanced_request = request;
 		}
-		return next.handle(enhanced_request).pipe(
-			map(response => response),
-			catchError((response: HttpErrorResponse) => {
-				switch(response.status) {
-					case 0:
-						this.router.navigate(['/network-error']);
-						break;
-					case 401:
-						this.router.navigate(['/login']);
-						break;
-				}
-				return throwError(response);
-			})
-		);
+		const response_stream = next.handle(enhanced_request);
+		//do not handle error if the the skip error flag has been set
+		if(flags[AuthInterceptor.HEADER_SKIP_ERROR_HANDLING]) {
+			return response_stream;
+		}
+		else {
+			return response_stream.pipe(
+				map(response => response),
+				catchError((response: HttpErrorResponse) => {
+					switch(response.status) {
+						case 0:
+							this.router.navigate(['/network-error']);
+							break;
+						case 401:
+							this.router.navigate(['/login']);
+							break;
+					}
+					return throwError(response);
+				})
+			);
+		}
 	}
 }
--- src/app/services/me.service.ts
+++ src/app/services/me.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpClient, HttpResponse, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AppService } from '../services/app.service';
 import { User } from '../models/user';
 import { PasswordUpdate } from '../models/password-update';
+import { AuthInterceptor } from 'app/auth.interceptor';
 
 @Injectable()
 export class MeService {
@@ -14,8 +15,7 @@ export class MeService {
 	}
 
 	changePassword(passwordUpdate: PasswordUpdate): Observable<HttpResponse<string>> {
-		const update = Object.assign({}, passwordUpdate);
-		delete update.repeatPassword;
-		return this.http.post<HttpResponse<string>>(AppService.API_URL + '/me/password', update);
+		const headers = new HttpHeaders().set(AuthInterceptor.HEADER_SKIP_ERROR_HANDLING, '');
+		return this.http.post<HttpResponse<string>>(AppService.API_URL + '/me/password', passwordUpdate, {headers});
 	}
 }
