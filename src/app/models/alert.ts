b24efa4cd1c87a43e42063a7aa6d573175dbd2e4
Matthieu Corageoud <matthieu.corageoud@gmail.com>
2022-01-03 23:19:50 +0100

    Enforce parameters required to create an alert

--- src/app/models/alert.ts
+++ src/app/models/alert.ts
@@ -1,8 +1,9 @@
 export class Alert {
-	message: string;
 	type: string;
+	message: string;
 
-	constructor(options: Alert) {
-		Object.assign(this, options);
+	constructor(type: string, message: string) {
+		this.type = type;
+		this.message = message;
 	}
 }
--- src/app/services/alert.service.ts
+++ src/app/services/alert.service.ts
@@ -7,14 +7,14 @@ export class AlertService {
 	private subject = new Subject<Alert>();
 
 	success(message: string) {
-		this.subject.next(new Alert({type: 'success', message}));
+		this.subject.next(new Alert('success', message));
 	}
 
 	error(message: string) {
-		this.subject.next(new Alert({type: 'error', message}));
+		this.subject.next(new Alert('error', message));
 	}
 
-	getAlerts(): Observable<any> {
+	getAlerts(): Observable<Alert> {
 		return this.subject.asObservable();
 	}
 }
