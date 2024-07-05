2900a7440f866d0c6bba07ac970ca2374697c711
Matthieu Corageoud <matthieu.corageoud@gmail.com>
2024-07-05 11:50:34 +0200

    Add a selector for deletion dialogs to avoid id collisions

--- src/app/dialogs/spot_deletion.dialog.ts
+++ src/app/dialogs/spot_deletion.dialog.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 
 @Component({
+	selector: 'app-spot-deletion-dialog',
 	templateUrl: './spot_deletion.dialog.html'
 })
 export class SpotDeletionDialog {}
--- src/app/dialogs/stash_deletion.dialog.ts
+++ src/app/dialogs/stash_deletion.dialog.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 
 @Component({
+	selector: 'app-stash-deletion-dialog',
 	templateUrl: './stash_deletion.dialog.html'
 })
 export class StashDeletionDialog {}
