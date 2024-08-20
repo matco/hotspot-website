e75843e4f247b138e75c8aa61a2233368263d020
Matthieu Corageoud <matthieu.corageoud@gmail.com>
2024-08-20 22:23:47 +0200

    Move application configuration in a dedicated file

---
+++ src/app/app.config.ts
@@ -0,0 +1,33 @@
+import {ApplicationConfig} from '@angular/core';
+import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+import {AuthInterceptor} from './auth.interceptor';
+import {AuthGuard} from './guards/authentication.guard';
+import {AppService} from './services/app.service';
+import {AlertService} from './services/alert.service';
+import {TokenService} from './services/token.service';
+import {MeService} from './services/me.service';
+import {UserService} from './services/user.service';
+import {StashService} from './services/stash.service';
+import {SpotService} from './services/spot.service';
+import {MapService} from './services/map.service';
+import {provideAnimations} from '@angular/platform-browser/animations';
+import {provideRouter} from '@angular/router';
+import {routes} from 'app/app.routes';
+
+export const appConfig: ApplicationConfig = {
+	providers: [
+		{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
+		AuthGuard,
+		AppService,
+		AlertService,
+		TokenService,
+		MeService,
+		UserService,
+		StashService,
+		SpotService,
+		MapService,
+		provideHttpClient(withInterceptorsFromDi()),
+		provideAnimations(),
+		provideRouter(routes),
+	]
+};
--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,12 @@
 import {enableProdMode} from '@angular/core';
-import {environment} from './environments/environment';
-import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
-import {AuthInterceptor} from './app/auth.interceptor';
-import {AuthGuard} from './app/guards/authentication.guard';
-import {AppService} from './app/services/app.service';
-import {AlertService} from './app/services/alert.service';
-import {TokenService} from './app/services/token.service';
-import {MeService} from './app/services/me.service';
-import {UserService} from './app/services/user.service';
-import {StashService} from './app/services/stash.service';
-import {SpotService} from './app/services/spot.service';
-import {MapService} from './app/services/map.service';
 import {bootstrapApplication} from '@angular/platform-browser';
-import {provideAnimations} from '@angular/platform-browser/animations';
+
+import {environment} from './environments/environment';
 import {AppComponent} from './app/app.component';
-import {provideRouter} from '@angular/router';
-import {routes} from 'app/app.routes';
+import {appConfig} from './app/app.config';
 
 if(environment.production) {
 	enableProdMode();
 }
 
-bootstrapApplication(AppComponent, {
-	providers: [
-		{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
-		AuthGuard,
-		AppService,
-		AlertService,
-		TokenService,
-		MeService,
-		UserService,
-		StashService,
-		SpotService,
-		MapService,
-		provideHttpClient(withInterceptorsFromDi()),
-		provideAnimations(),
-		provideRouter(routes),
-	]
-});
+bootstrapApplication(AppComponent, appConfig).catch(e => console.error(e));
